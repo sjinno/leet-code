fb419287d95d5f4ce5117c25f76e494406966235
Shohei Jinno <shohei.jinno@gmail.com>
2021-02-06 13:22:20 -0800

    17. Letter Combinations of a Phone NUmber: Done.

--- src/bin/q-017.rs
+++ src/bin/q-017.rs
@@ -44,3 +44,56 @@
 //                         tmp.append(ch + c)
 
 //         return ans if tmp == [] else tmp
+
+fn letter_combinations(digits: String) -> Vec<String> {
+    if digits.is_empty() {
+        return vec![];
+    }
+
+    let mut stack = vec![];
+    digits.chars().into_iter().for_each(|d| match d {
+        '2' => stack.push("abc"),
+        '3' => stack.push("def"),
+        '4' => stack.push("ghi"),
+        '5' => stack.push("jkl"),
+        '6' => stack.push("mno"),
+        '7' => stack.push("pqrs"),
+        '8' => stack.push("tuv"),
+        '9' => stack.push("wxyz"),
+        _ => (),
+    });
+
+    let mut ans = Vec::<String>::new();
+    let last_elt = stack.pop().unwrap();
+    let mut tmp = last_elt
+        .chars()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>();
+    if stack.is_empty() {
+        return tmp;
+    }
+
+    while let Some(s) = stack.pop() {
+        if !tmp.is_empty() {
+            while let Some(c) = tmp.pop() {
+                s.chars().into_iter().for_each(|ch| {
+                    let combination = format!("{}{}", ch, c);
+                    ans.push(combination);
+                });
+            }
+        } else {
+            while let Some(c) = ans.pop() {
+                s.chars().into_iter().for_each(|ch| {
+                    let combination = format!("{}{}", ch, c);
+                    tmp.push(combination);
+                });
+            }
+        }
+    }
+
+    if tmp.is_empty() {
+        ans
+    } else {
+        tmp
+    }
+}
